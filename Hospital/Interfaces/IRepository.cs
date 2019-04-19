1140c6ad21989fa83433211a34b984a38ea2b88e
a.d.peshkov <a.d.peshkov@UK-RND-696>
2019-04-19 15:47:00 +0700

    Add: IRepository, IEntity

---
+++ Hospital/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    public interface IRepository<T> where T: IEntity 
+    {
+        T GetById(long Id);
+        void Create(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+
+
+
+    }
+}
